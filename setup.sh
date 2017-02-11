5af7f2966b3a75b88fffe5822c0448dfcf6cb1f3
dojineko <dojineko.spiral@gmail.com>
2017-02-11 16:19:04 +0900

    setup処理を変更

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-BASE=$(cd $(dirname $0) && pwd)
-cd ${BASE}
+BASEDIR=$(cd $(dirname $0) && pwd)
+cd ${BASEDIR}
 
 SCRIPT_DIR="${HOME}/Library/Application Support/iTerm/Scripts"
 [[ -d "${SCRIPT_DIR}" ]] || mkdir -p "${SCRIPT_DIR}"
 
 for i in $(ls -1 *.js); do
   SRC=$(basename ${i%.js})
-  osacompile -l JavaScript -o ./${SRC}.scpt ./${SRC}.js
-  ln -fs "${BASE}/${SRC}.scpt" "${SCRIPT_DIR}/${SRC}.scpt"
+  DEST="${SCRIPT_DIR}/${SRC}.scpt"
+  [[ -e "${DEST}" ]] && /bin/rm -f "${DEST}"
+  osacompile -l JavaScript -o "${DEST}" "./${i}"
 done
